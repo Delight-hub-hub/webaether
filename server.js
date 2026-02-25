72c9ddef45bd74d1f4c717bf8dd7474aeedb3ba7
Delight <delightking03@gmail.com>
2026-02-25 10:03:53 +0200

    h

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ const clientRoutes = require("./routes/clientRoutes");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://aether-frontend-rho.vercel.app/"
+];
+
+
 //middleware
-app.use(cors({origin: 'http://localhost:5173'}));
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use(express.json());
 app.use("/api/clients", clientRoutes);
 
