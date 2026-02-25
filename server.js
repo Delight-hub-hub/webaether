cd8164e35b2df05d1ee9bae019ce20403736c157
Delight <delightking03@gmail.com>
2026-02-25 08:50:24 +0200

    initial backend commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const clientRoutes = require("./routes/clientRoutes");
+
+const app = express();
+
+//middleware
+app.use(cors({origin: 'http://localhost:5173'}));
+app.use(express.json());
+app.use("/api/clients", clientRoutes);
+
+//connect database
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Aether Systems API');
+})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
