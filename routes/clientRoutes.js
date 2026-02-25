aa0f3ff867431130e964e203b4e2992d08d2956a
Delight <delightking03@gmail.com>
2026-02-25 09:03:29 +0200

    initial commit

---
+++ routes/clientRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Client = require("../models/\client");
+
+// CREATE client
+router.post("/", async (req, res) => {
+  try {
+    const newClient = new Client(req.body);
+    const savedClient = await newClient.save();
+    res.status(201).json(savedClient);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// GET all clients
+router.get("/", async (req, res) => {
+  try {
+    const clients = await Client.find().sort({ createdAt: -1 });
+    res.json(clients);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
